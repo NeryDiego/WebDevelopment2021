d2209e1dbaba6ef38397b5ed74aceb1e95dbc337
Diego Nery <1896457@tcs.com>
2021-03-16 17:31:05 -0600

    Concatenation example

---
+++ Leccion 14/concatenation.js
@@ -0,0 +1,9 @@
+var nombre = "Diego";
+var apellido = "Martinez";
+var telefono = "3314798479";
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var x = nombre + ' ' + 128;
+console.log(x);
