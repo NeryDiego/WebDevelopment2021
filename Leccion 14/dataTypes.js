fdea42cb18306a8edfb72d5b8da2fce4c885408b
Diego Nery <1896457@tcs.com>
2021-03-16 17:09:46 -0600

    Se creo un archivo para observar los tipos de datos

---
+++ Leccion 14/dataTypes.js
@@ -0,0 +1,15 @@
+var stringVar = "Diego";
+console.log(stringVar);
+
+var intVar = 10;
+console.log(intVar);
+
+//Tipo de dato object
+var objeto = {
+    nombre : "Diego",
+    edad: "24",
+    trabajo: "IT Services"
+};
+
+console.log(objeto);
+console.log(objeto.edad);
