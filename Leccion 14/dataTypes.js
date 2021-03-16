02ff2ecd41c8edb975eac29a26371b3b30cd2128
Diego Nery <1896457@tcs.com>
2021-03-16 17:18:34 -0600

    Symbol, null, undefined....

--- Leccion 14/dataTypes.js
+++ Leccion 14/dataTypes.js
@@ -17,3 +17,35 @@ var objeto = {
 console.log(objeto);
 console.log(objeto.edad);
 console.log(typeof(objeto));
+
+//Tipo de dato boolean 
+var flag = true;
+console.log(flag + " Type of:" + typeof(flag));
+
+//Tipo de dato funcion
+function suma(a, b){
+    return a+b;
+}
+
+console.log(suma(5,30) + " Type of: " + typeof(suma))
+
+//Tipo de dato Symbol
+var symbol = Symbol("mi simbolo"); //Valor unico de una variable
+console.log(typeof(symbol))
+
+//Tipo clase es una funcion
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//Tipo undefined
+var x;
+console.log(typeof(x));
+
+//Null = ausencia de valor
+var y = null;
+console.log(y);
+console.log(typeof y);
