91007348081473ff8763dc90b9534faf27df909d
Diego Nery <1896457@tcs.com>
2021-03-16 17:42:50 -0600

    Creacion variables.js

---
+++ Leccion 14/variables.js
@@ -0,0 +1,11 @@
+/*
+const --> Declara una constante
+var --> Ya no se utiliza para declarar una variable
+let --> Se utiliza en lugar de var
+*/
+
+//En JS se puede declarar una variable solamente utilizando el nombre
+nombre = "Diego";
+let number = 10;
+const lada = 33;
+lada = 10; //No se puede cambiar una variable que es const
