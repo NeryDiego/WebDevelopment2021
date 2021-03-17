d5f82bd4bd198f3b34a8e78b034e67773ed23fff
Diego Nery <1896457@tcs.com>
2021-03-17 11:24:52 -0600

    Ciclo FOR con Break;

--- Leccion16Ciclos/for.js
+++ Leccion16Ciclos/for.js
@@ -1,5 +1,10 @@
 
 let array = [1,2,3,4];
 for(let count = 0; count <= 3; count++){
-    console.log(array[count]);
+    if( array[count] % 2 == 0){
+        console.log("Valor par: " + array[count]);
+        break; //Una vez que encontremos el primer numero par, esto terminará el ciclo for
+    }
 }
+
+console.log("Fin ciclo For");
