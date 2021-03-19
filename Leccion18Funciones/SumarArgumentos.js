8434c0663e4074544dce79bcd393b042eb80e5e9
Diego Nery <1896457@tcs.com>
2021-03-19 10:39:50 -0600

    Sumar todos los argumentos utilizando keyword -arguments-

---
+++ Leccion18Funciones/SumarArgumentos.js
@@ -0,0 +1,12 @@
+let resultado = sumarTodo(5,4,13,10,9);
+
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma = suma + arguments[i];
+    }
+
+    return suma;
+}
+
+console.log(resultado);
