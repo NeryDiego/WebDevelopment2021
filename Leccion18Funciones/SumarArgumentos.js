32b949f6b90e8eae9d14ca817e5b643fdea1d951
Diego Nery <1896457@tcs.com>
2021-03-19 10:49:46 -0600

    Paso por valor y paso por referencia a funcion

--- Leccion18Funciones/SumarArgumentos.js
+++ Leccion18Funciones/SumarArgumentos.js
@@ -10,3 +10,27 @@ function sumarTodo(){
 }
 
 console.log(resultado);
+
+// Paso por valor
+let x = 10; //Valor primitivo
+
+function cambiarValor(a){
+    a = 20;
+}
+
+cambiarValor(x);
+console.log(x);
+
+// Paso por referencia
+const persona = {
+    nombre : "Diego",
+    apellido : "Martinez",
+    edad : 24
+}
+
+function cambiarValorObjecto(p1){
+    p1.nombre = 'Carlos';
+}
+
+cambiarValorObjecto(persona);
+console.log(persona);
