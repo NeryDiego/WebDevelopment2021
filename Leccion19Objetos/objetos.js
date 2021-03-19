35fba54c68824c842091547efdfb59a2a5060702
Diego Nery <1896457@tcs.com>
2021-03-19 11:04:27 -0600

    Recorrer las propiedes del objeto

--- Leccion19Objetos/objetos.js
+++ Leccion19Objetos/objetos.js
@@ -1,3 +1,4 @@
+// Forma común de definir objetos
 let persona = {
     nombre : 'Diego',
     apellido : "Martinez",
@@ -8,3 +9,19 @@ let persona = {
 }
 
 console.log(persona.nombreCompleto());
+
+// Definir objeto de otra manera
+let persona2 = new Object();
+persona2.nombre = "Juan";
+persona2.apellido = "Perez";
+persona2.telefono = "No";
+
+console.log(persona2);
+console.log(persona2 ['apellido']);
+
+for (const key in persona2) {
+    if (Object.hasOwnProperty.call(persona2, key)) {
+        console.log(persona2[key]);
+        
+    }
+}
