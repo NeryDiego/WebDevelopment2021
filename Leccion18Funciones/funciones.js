c9e6468484570627f824aeea2c00d5bd32ffd00e
Diego Nery <1896457@tcs.com>
2021-03-19 10:33:43 -0600

    Funciones flecha, parametros y argumentos

--- Leccion18Funciones/funciones.js
+++ Leccion18Funciones/funciones.js
@@ -1,9 +1,12 @@
 //Funciones normales
 function suma(a,b){
+    console.log(arguments[0]);
+    console.log(arguments[1]);
     console.log(a+b);
 }
 
 suma(5,6);
+suma(5);
 
 //Declaracion de función de tipo expresión
 let x = function (a,b){return a + b};
@@ -15,3 +18,8 @@ console.log(resultado);
 (function (a,b){
     console.log("Funcion Self Invoking, a+b=" + (a+b));
 })(3,5); //Estas funciones no se pueden reutilizar
+
+//Funciones de tipo flecha
+const sumarFuncionesTipoFlecha = (a, b) => a + b;
+let sumaFlecha = sumarFuncionesTipoFlecha(3,8);
+console.log(sumaFlecha);
